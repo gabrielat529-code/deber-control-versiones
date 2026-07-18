14f34db47fcf84ffc3cf14098f44455240b24a8a
Gabriela Torres <gabrielat529@gmail.com>
2026-07-17 21:55:29 -0500

    v1.2: Se agrego recuperar contrasena

--- Login.java
+++ Login.java
@@ -1,5 +1,6 @@
 public class Login {
     public static void main(String[] args) {
         System.out.println("Validación de usuario lista.");
+        System.out.println("Funcionalidad: Recuperar contraseña añadida.");
     }
 }
